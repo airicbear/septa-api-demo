842a1094653e7311d9bcef57983d5bc18ddd36a1
Eric Nguyen <etndevel@gmail.com>
2024-04-28 01:35:35 -0400

    Add title

--- app/page.tsx
+++ app/page.tsx
@@ -31,29 +31,36 @@ export default function Page() {
   }
 
   return (
-    <main className="bg-gray-500 max-w-xl my-6 mx-auto p-6 rounded-md">
-      <div className="mb-2">
-        <p className="font-bold mr-2 text-gray-300">URL</p>
+    <main>
+      <div className="max-w-xl my-6 mx-auto p-6">
+        <div>
+          <p className="font-bold text-4xl text-gray-950 mb-2">SEPTA API Demo</p>
+        </div>
+        <div className="bg-gray-500 p-6 rounded-md">
+          <div className="mb-2">
+            <p className="font-bold mr-2 text-gray-300">URL</p>
+          </div>
+          <div className="bg-gray-100 p-2 mb-2 rounded-md">
+            <Link href={url}>
+              {url}
+            </Link>
+          </div>
+          <div className="mb-2">
+            <p className="font-bold mr-2 text-gray-300">Endpoint</p>
+          </div>
+          <div>
+            <select id="endpoint" value={selectedEndpoint} onChange={handleEndpointSelect}
+              className="mb-4 text-gray-700 block w-full px-4 py-2 bg-gray-400 border border-gray-400 rounded-md shadow-sm focus:outline-none focus:border-gray-500 focus:ring focus:ring-gray-400"
+            >
+              {SEPTA_API_ALL_DATA.map((endpoint) => (<option key={endpoint} value={endpoint}>{endpoint}</option>))}
+            </select>
+          </div>
+          {
+            (selectedEndpoint == SEPTA_API_ALL_DATA.at(0)) ?
+              <ArrivalsInputComponent handleChange={handleParamsChange} /> : <></>
+          }
+        </div>
       </div>
-      <div className="bg-gray-100 p-2 rounded-md">
-        <Link href={url}>
-          {url}
-        </Link>
-      </div>
-      <div className="mb-2">
-        <p className="font-bold mr-2 text-gray-300">Endpoint</p>
-      </div>
-      <div>
-        <select id="endpoint" value={selectedEndpoint} onChange={handleEndpointSelect}
-          className="mb-4 text-gray-700 block w-full px-4 py-2 bg-gray-400 border border-gray-400 rounded-md shadow-sm focus:outline-none focus:border-gray-500 focus:ring focus:ring-gray-400"
-        >
-          {SEPTA_API_ALL_DATA.map((endpoint) => (<option key={endpoint} value={endpoint}>{endpoint}</option>))}
-        </select>
-      </div>
-      {
-        (selectedEndpoint == SEPTA_API_ALL_DATA.at(0)) ?
-          <ArrivalsInputComponent handleChange={handleParamsChange} /> : <></>
-      }
     </main>
   );
 }
